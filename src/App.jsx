0d73d76af60a2c2f27e757d9a8009810775d1125
Ruben <ruben_picos10@hotmail.com>
2024-05-23 16:26:47 +0200

    Basic structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import SearchBtn from "./components/SearchBtn";
+
+function App() {
+  return (
+    <>
+      <section className="container">
+        <h1 className="container__title">The weather app</h1>
+        <SearchBtn />
+      </section>
+    </>
+  );
+}
+
+export default App;
