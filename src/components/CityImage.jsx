593dffdd5f757860b0af202cf9cc810dacadf631
Ruben <ruben_picos10@hotmail.com>
2024-06-20 14:40:52 +0200

    Delete bad component and create a new for the new API call

--- src/components/CityImage.jsx
+++ src/components/CityImage.jsx
@@ -1,23 +1,24 @@
-const getCityImage = async (cityName) => {
+const CityImage = async (cityName) => {
   const unsplashAccessKey = "jg0iFWjX3NAVoNtWkp9IdtD7768uM9nIYL3-lyTKjMw";
   const url = `https://api.unsplash.com/search/photos?query=${cityName}&per_page=1&client_id=${unsplashAccessKey}`;
 
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error("Error al obtener la imagen");
+      throw new Error("Cant shoe the image");
     }
     const data = await response.json();
+    console.log(data);
 
     if (data.results.length === 0) {
-      throw new Error("No se encontró ninguna imagen para esta ciudad");
+      throw new Error("Cant show the image");
     }
 
     return data.results[0].urls.regular;
   } catch (error) {
-    console.error("Error al obtener la imagen de la ciudad:", error);
+    console.error("Cant shoe the image:", error);
     return null;
   }
 };
 
-export default getCityImage;
+export default CityImage;
